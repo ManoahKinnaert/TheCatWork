cc0a41e8fb87659d5c7ed3f679aabfb1d339ba18
Manoah Kinnaert <manoah.k@icloud.com>
2026-07-16 12:37:23 +0200

    Random fact stuff

--- CatWork/js/index.js
+++ CatWork/js/index.js
@@ -1,6 +1,6 @@
 let table = document.getElementById("picture-table")
 let catPicBtn = document.getElementById("cat-pic-refresh-btn")
-
+let randomFactText = document.getElementById("random-fact-text")
 
 function renderCatPictureTable(images) {
     let html = ""
@@ -18,11 +18,14 @@ const updateCatPictures = async () => {
     // get 9 images
     const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=9')
     const json = await response.json()
-    console.log(json)
     // get the images themselves
     let images = []
     for (let img of json) images.push(img["url"])
     renderCatPictureTable(images)
+    // also get a random cat fact
+    const response2 = await fetch("https://meowfacts.herokuapp.com/")
+    const json2 = await response2.json()
+    randomFactText.innerText = json2["data"][0];
 }
 // handle events
 catPicBtn.addEventListener("click", updateCatPictures)
